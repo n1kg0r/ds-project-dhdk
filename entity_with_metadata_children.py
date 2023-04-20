53b489751c9d1af370ed6ee1f9f609ef43ae99d1
n1kg0r <n1kg0r.public@gmail.com>
2023-04-20 11:48:02 +0200

    small fix at entity_with_metadata_children

--- entity_with_metadata_children.py
+++ entity_with_metadata_children.py
@@ -1,22 +1,22 @@
 from EntityWithMetadata import EntityWithMetadata
 
 class Canvas(EntityWithMetadata):
-    def __init__(self, id:str, label, title, creators):
-        pass
+    def __init__(self, id:str, label:str, title:str, creators:list[str]):
+        super().__init__(id, label, title, creators)
 
 class Manifest(EntityWithMetadata):
-    def __init__(self, id:str, label, title, creators, items:list[Canvas]):
+    def __init__(self, id:str, label:str, title:str, creators:list[str], items:list[Canvas]):
         super().__init__(id, label, title, creators)
         self.items = items
     def getItems(self):
         return self.items
 
 class Collection(EntityWithMetadata):
-    def __init__(self, id:str, label, title, creators, items:list[Manifest]):
+    def __init__(self, id:str, label:str, title:str, creators:list[str], items:list[Manifest]):
         super().__init__(id, label, title, creators)
         self.items = items
     def getItems(self):
         return self.items
 
-# c = Canvas('a', 'b', 'c', [1,2,3])
+# c = Canvas(10.5, 'b', 'c', [1,2,3])
 # print(c.creators)
