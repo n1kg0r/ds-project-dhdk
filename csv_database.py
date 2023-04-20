e0bf2ea6f39dc80cdc4a3bbc70dcd888da58b363
n1kg0r <n1kg0r.public@gmail.com>
2023-04-20 15:26:14 +0200

    url changed at csv_database

--- csv_database.py
+++ csv_database.py
@@ -2,7 +2,7 @@ from pandas import read_csv, Series, DataFrame
 from sqlite3 import connect
 
 
-annotations = read_csv("https://github.com/n1kg0r/ds-project-dhdk/tree/main/data/annotations.csv", 
+annotations = read_csv("./data/annotations.csv", 
                         keep_default_na=False,
                         dtype={
                             "id": "string",
@@ -15,7 +15,7 @@ image = annotations[["body"]]
 image = image.rename(columns={"body": "id"})
 
 
-entityWithMetadata= read_csv("https://github.com/n1kg0r/ds-project-dhdk/tree/main/data/metadata.csv", 
+entityWithMetadata= read_csv("./data/metadata.csv", 
                         keep_default_na=False,
                         dtype={
                             "id": "string",
@@ -24,7 +24,7 @@ entityWithMetadata= read_csv("https://github.com/n1kg0r/ds-project-dhdk/tree/mai
                         })
 
 
-with connect("https://github.com/n1kg0r/ds-project-dhdk/tree/main/data/annotation.db") as con:
+with connect("./data/annotation.db") as con:
     annotations.to_sql("Annotation", con, if_exists="replace", index=False)
     image.to_sql("Image", con, if_exists="replace", index=False)
     entityWithMetadata.to_sql("Entity", con, if_exists="replace", index=False)
