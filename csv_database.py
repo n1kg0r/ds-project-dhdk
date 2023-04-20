e1169dffabf9f3e457d7bda07601445504c32e8d
EriA <83347847+EricaAndreose@users.noreply.github.com>
2023-04-20 15:09:25 +0200

    Update csv_database.py
    changes of path

--- csv_database.py
+++ csv_database.py
@@ -1,9 +1,8 @@
 from pandas import read_csv, Series, DataFrame
 from sqlite3 import connect
 
-# maybe replace path with: https://github.com/n1kg0r/ds-project-dhdk/tree/main/data/annotations.csv
 
-annotations = read_csv("C:/Users/NIKI/Desktop/Data - Peroni/Progetto/annotations.csv", 
+annotations = read_csv("https://github.com/n1kg0r/ds-project-dhdk/tree/main/data/annotations.csv", 
                         keep_default_na=False,
                         dtype={
                             "id": "string",
@@ -15,9 +14,8 @@ annotations = read_csv("C:/Users/NIKI/Desktop/Data - Peroni/Progetto/annotations
 image = annotations[["body"]]
 image = image.rename(columns={"body": "id"})
 
-# maybe replace path with: https://github.com/n1kg0r/ds-project-dhdk/tree/main/data/metadata.csv
 
-entityWithMetadata= read_csv("C:/Users/NIKI/Desktop/Data - Peroni/Progetto/metadata.csv", 
+entityWithMetadata= read_csv("https://github.com/n1kg0r/ds-project-dhdk/tree/main/data/metadata.csv", 
                         keep_default_na=False,
                         dtype={
                             "id": "string",
@@ -25,9 +23,8 @@ entityWithMetadata= read_csv("C:/Users/NIKI/Desktop/Data - Peroni/Progetto/metad
                             "creator": "string"
                         })
 
-# maybe replace path with: https://github.com/n1kg0r/ds-project-dhdk/tree/main/data/annotation.db
 
-with connect("C:/Users/NIKI/Desktop/Data - Peroni/Progetto/annotation.db") as con:
+with connect("https://github.com/n1kg0r/ds-project-dhdk/tree/main/data/annotation.db") as con:
     annotations.to_sql("Annotation", con, if_exists="replace", index=False)
     image.to_sql("Image", con, if_exists="replace", index=False)
     entityWithMetadata.to_sql("Entity", con, if_exists="replace", index=False)
