8aa8123e61dfa3d9c319d9f8d5cf91f025568c4a
EriA <83347847+EricaAndreose@users.noreply.github.com>
2023-04-24 12:26:50 +0200

    Add files via upload

--- Collection_processor.py
+++ Collection_processor.py
@@ -35,7 +35,6 @@ class CollectionProcessor(Processor):
 
     def uploadData(self, path: str):
         my_graph = Graph()
-        store = SPARQLUpdateStore()
 
         if path:
 
@@ -43,58 +42,69 @@ class CollectionProcessor(Processor):
             with open(path, mode='r', encoding="utf-8") as jsonfile:
                 json_object = json.load(jsonfile)
 
-            # iterate over it: first step is entering the collection dictionary
-            for collection in json_object:
-                collection_id = json_object['id']
+                # iterate over it: first step is entering the collection dictionary
+                for collection in json_object:
+                    collection_id = json_object['id']
 
-                collection_node = URIRef(collection_id)
-                my_graph.add((collection_node, RDF.type, Collection))
-                my_graph.add((collection_node, ids, Literal(collection_id)))
-                my_graph.add((collection_node, label, Literal(json_object['label']['none'][0])))
+                    collection_node = URIRef(collection_id)
+                    my_graph.add((collection_node, RDF.type, Collection))
+                    my_graph.add((collection_node, ids, Literal(collection_id)))
+                    my_graph.add((collection_node, label, Literal(json_object['label']['none'][0])))
 
-                # second step is entering the collection items list
-                # here i take the id
-                for manifest in json_object["items"]:
-                    manifest_id = manifest['id']
+                    # second step is entering the collection items list
+                    # here i take the id
+                    for manifest in json_object["items"]:
+                        manifest_id = manifest['id']
 
-                    # create the Manifest node with its attributes
-                    manifest_node = URIRef(manifest_id)
-                    my_graph.add((manifest_node, RDF.type, Manifest))
-                    my_graph.add((manifest_node, ids, Literal(manifest_id)))
-                    my_graph.add((manifest_node, label, Literal(manifest['label']['none'][0])))
-                        
-                    # third step is entering the manifest items list
-                    # here i take the id and the label (entering label's dictionary)
-                    for item in manifest["items"]:
-                        canvas_id = item['id']
-                        canvas_label = item['label']['none'][0]
+                        # create the Manifest node with its attributes
+                        manifest_node = URIRef(manifest_id)
+                        my_graph.add((manifest_node, RDF.type, Manifest))
+                        my_graph.add((manifest_node, ids, Literal(manifest_id)))
+                        my_graph.add((manifest_node, label, Literal(manifest['label']['none'][0])))
 
-                        # create the Canvas node with its attributes
-                        canvas_node = URIRef(canvas_id)
-                        my_graph.add((canvas_node, RDF.type, Canvas))
-                        my_graph.add((canvas_node, ids, Literal(item['id'])))
-                        my_graph.add((canvas_node, label, Literal(canvas_label)))
+                        # third step is entering the manifest items list
+                        # here i take the id and the label (entering label's dictionary)
+                        for item in manifest["items"]:
+                            canvas_id = item['id']
+                            canvas_label = item['label']['none'][0]
 
-                        # create the items relation between the Manifest and the Canvas
-                        my_graph.add((collection_node, items, manifest_node))
-                        my_graph.add((manifest_node, items, canvas_node))
+                            # create the Canvas node with
+                            canvas_node = URIRef(canvas_id)
+                            my_graph.add((canvas_node, RDF.type, Canvas))
+                            my_graph.add((canvas_node, ids, Literal(item['id'])))
+                            my_graph.add((canvas_node, label, Literal(canvas_label)))
+
+                # create the items relation between the Manifest and the Canvas
+                my_graph.add((collection_node, items, manifest_node))
+                my_graph.add((manifest_node, items, canvas_node))
+
+            store_json = SPARQLUpdateStore()
 
             endpoint = self.dbPathOrUrl
 
-            store.open((endpoint, endpoint))
+            store_json.open((endpoint, endpoint))
 
             for triple in my_graph.triples((None, None, None)):
-                store.add(triple)
+                store_json.add(triple)
 
-            store.close()
+            store_json.close()
 
             return True 
         else: 
             return False
         
 
-col_proc = CollectionProcessor()
-col_proc.dbPathOrUrl = "http://192.168.0.168:9999/blazegraph/sparql"
-col_proc.uploadData("collection-1.json")
+# try1 = CollectionProcessor()
+# try1.dbPathOrUrl = "http://192.168.0.168:9999/blazegraph/sparql"
+# try1.getdbPathOrUrl()
+# try1.uploadData("collection-1.json")
+
+# print(try1.uploadData("collection-1.json"))
 
 
+# try2 = CollectionProcessor()
+# try2.dbPathOrUrl = "http://192.168.0.168:9999/blazegraph/sparql"
+# try2.getdbPathOrUrl()
+# try2.uploadData("collection-2.json")
+
+# print(try2.uploadData("collection-2.json"))
