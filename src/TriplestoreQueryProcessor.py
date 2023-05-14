14ae35758406e3aeea053ba363073b19d508b8d3
EriA <83347847+EricaAndreose@users.noreply.github.com>
2023-05-14 16:36:51 +0200

    Add files via upload

--- src/TriplestoreQueryProcessor.py
+++ src/TriplestoreQueryProcessor.py
@@ -4,14 +4,14 @@ from pandas import DataFrame
 from sparql_dataframe import get 
 from clean_str import remove_special_chars
 
-class TriplestoreQueryProcessor(QueryProcessor):
+class TriplestoreQueryProcessor(Processor):
 
     def __init__(self):
         super().__init__()
 
     def getAllCanvases(self):
 
-        endpoint = Processor.getDbPathOrUrl
+        endpoint = self.getDbPathOrUrl()
         query_canvases = """
         PREFIX ns1: <https://github.com/n1kg0r/ds-project-dhdk/attributes/> 
         PREFIX ns2: <http://purl.org/dc/elements/1.1/> 
@@ -30,7 +30,7 @@ class TriplestoreQueryProcessor(QueryProcessor):
 
     def getAllCollections(self):
 
-        endpoint = Processor.getDbPathOrUrl
+        endpoint = self.getDbPathOrUrl()
         query_collections = """
         PREFIX ns1: <https://github.com/n1kg0r/ds-project-dhdk/attributes/> 
         PREFIX ns2: <http://purl.org/dc/elements/1.1/> 
@@ -49,7 +49,7 @@ class TriplestoreQueryProcessor(QueryProcessor):
 
     def getAllManifest(self):
 
-        endpoint = Processor.getDbPathOrUrl
+        endpoint = self.getDbPathOrUrl()
         query_manifest = """
         PREFIX ns1: <https://github.com/n1kg0r/ds-project-dhdk/attributes/> 
         PREFIX ns2: <http://purl.org/dc/elements/1.1/> 
@@ -68,7 +68,7 @@ class TriplestoreQueryProcessor(QueryProcessor):
 
     def getCanvasesInCollection(self, collectionId: str):
 
-        endpoint = Processor.setDbPathOrUrl
+        endpoint = self.setDbPathOrUrl()
         query_canInCol = """
         PREFIX ns1: <https://github.com/n1kg0r/ds-project-dhdk/attributes/> 
         PREFIX ns2: <http://purl.org/dc/elements/1.1/> 
@@ -92,7 +92,7 @@ class TriplestoreQueryProcessor(QueryProcessor):
 
     def getCanvasesInManifest(self, manifestId: str):
 
-        endpoint = Processor.getDbPathOrUrl
+        endpoint = self.getDbPathOrUrl()
         query_canInMan = """
         PREFIX ns1: <https://github.com/n1kg0r/ds-project-dhdk/attributes/> 
         PREFIX ns2: <http://purl.org/dc/elements/1.1/> 
@@ -113,9 +113,9 @@ class TriplestoreQueryProcessor(QueryProcessor):
         return df_sparql_getCanvasesInManifest
 
 
-    def getManifestInCollection(self, collectionId: str):
+    def getManifestsInCollection(self, collectionId: str):
 
-        endpoint = Processor.getDbPathOrUrl
+        endpoint = self.getDbPathOrUrl()
         query_manInCol = """
         PREFIX ns1: <https://github.com/n1kg0r/ds-project-dhdk/attributes/> 
         PREFIX ns2: <http://purl.org/dc/elements/1.1/> 
@@ -139,32 +139,92 @@ class TriplestoreQueryProcessor(QueryProcessor):
     def getEntitiesWithLabel(self, label: str): 
             
 
-        endpoint = "http://192.168.1.55:9999/blazegraph/sparql"
+        endpoint = self.getDbPathOrUrl()
         query_entityLabel = """
         PREFIX ns1: <https://github.com/n1kg0r/ds-project-dhdk/attributes/> 
         PREFIX ns2: <http://purl.org/dc/elements/1.1/> 
         PREFIX ns3: <https://github.com/n1kg0r/ds-project-dhdk/relations/> 
 
-        SELECT ?entity ?type ?id
+        SELECT ?entity ?type ?label ?id
         WHERE {
             ?entity ns1:label "%s" ;
             a ?type ;
+            ns1:label ?label ;
             ns2:identifier ?id .
         }
         """ % remove_special_chars(label)
 
         df_sparql_getEntitiesWithLabel = get(endpoint, query_entityLabel, True)
         return df_sparql_getEntitiesWithLabel
+    
+
+    def getEntitiesWithCanvas(self, canvasId: str): 
+            
+        endpoint = self.getDbPathOrUrl()
+        query_entityCanvas = """
+        PREFIX ns1: <https://github.com/n1kg0r/ds-project-dhdk/attributes/> 
+        PREFIX ns2: <http://purl.org/dc/elements/1.1/> 
+        PREFIX ns3: <https://github.com/n1kg0r/ds-project-dhdk/relations/> 
+
+        SELECT ?id ?label ?type
+        WHERE {
+            ?entity ns2:identifier "%s" ;
+            ns2:identifier ?id ;
+            ns1:label ?label ;
+            a ?type .
+        }
+        """ % remove_special_chars(canvasId)
+
+        df_sparql_getEntitiesWithCanvas = get(endpoint, query_entityCanvas, True)
+        return df_sparql_getEntitiesWithCanvas
+    
+    def getEntitiesWithId(self, id: str): 
+            
+        endpoint = self.getDbPathOrUrl()
+        query_entityId = """
+        PREFIX ns1: <https://github.com/n1kg0r/ds-project-dhdk/attributes/> 
+        PREFIX ns2: <http://purl.org/dc/elements/1.1/> 
+        PREFIX ns3: <https://github.com/n1kg0r/ds-project-dhdk/relations/> 
+
+        SELECT ?id ?label ?type
+        WHERE {
+            ?entity ns2:identifier "%s" ;
+            ns2:identifier ?id ;
+            ns1:label ?label ;
+            a ?type .
+        }
+        """ % remove_special_chars(id)
+
+        df_sparql_getEntitiesWithId = get(endpoint, query_entityId, True)
+        return df_sparql_getEntitiesWithId
+    
+
+    def getAllEntities(self): 
+            
+        endpoint = self.getDbPathOrUrl()
+        query_AllEntities = """
+        PREFIX ns1: <https://github.com/n1kg0r/ds-project-dhdk/attributes/> 
+        PREFIX ns2: <http://purl.org/dc/elements/1.1/> 
+        PREFIX ns3: <https://github.com/n1kg0r/ds-project-dhdk/relations/> 
+
+        SELECT *
+        WHERE {
+            ?entity ns2:identifier ?id .
+        }
+        """ 
+
+        df_sparql_getAllEntities = get(endpoint, query_AllEntities, True)
+        return df_sparql_getAllEntities
 
 
 
 
 # create an instance of the TriplestoreQueryProcessor class
-query_processor = TriplestoreQueryProcessor()
+# Trp_query_processor = TriplestoreQueryProcessor()
 
 # call the getAllCanvases method to retrieve the canvases from the triplestore
-entity_df = query_processor.getEntitiesWithLabel('Raimondi, Giuseppe. Quaderno manoscritto, "Caserma Scalo : 1930-1968"')
-entity_dt = query_processor.getEntitiesWithLabel("Raimondi, Giuseppe. Quaderno manoscritto, \"Caserma Scalo : 1930-1968\"")
+# entity_df = Trp_query_processor.getEntitiesWithLabel('Raimondi, Giuseppe. Quaderno manoscritto, "Caserma Scalo : 1930-1968"')
+# entity_dt = Trp_query_processor.getEntitiesWithLabel("Raimondi, Giuseppe. Quaderno manoscritto, \"Caserma Scalo : 1930-1968\"")
 # print the dataframe containing the canvases
-print(entity_df)
-print(entity_dt)
+# print(entity_df)
+# print(entity_dt)
